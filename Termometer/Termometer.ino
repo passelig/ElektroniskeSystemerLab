69098449c17f30f2660c20a96b3b3015bf51aac2
Gunnar Storebø <gunnar.storebo@vlfk.no>
2023-11-02 12:33:17 +0100

    initial commit

---
+++ Termometer/Termometer.ino
@@ -0,0 +1,29 @@
+int highValueFromADconverter = 642;
+int lowValueFromADconverter = 462;
+
+int highReferenceVoltage = 278; // 278 betyr 2,78V
+int lowReferenceVoltage = 234;
+
+int highReferenceTemperature = 223; // 223 betyr 22,3 grader
+int lowReferenceTemperature = 100; // 100 betyr 10,0 grader
+
+void setup() {
+  // Denne trenger vi for å få data tilbake til pc’en på serial monitor
+  Serial.begin(9600);
+}
+void loop() {
+  // Les analog verdi fra Analog Inngangsport A0
+  int valueFromADConverter= analogRead(A0);
+  // Skriv analog verdien til serieporten
+
+  float temperature = map(valueFromADConverter, lowValueFromADconverter, highValueFromADconverter, lowReferenceTemperature, highReferenceTemperature)/(float)(10);
+  float voltage = map(valueFromADConverter, lowValueFromADconverter, highValueFromADconverter, lowReferenceVoltage, highReferenceVoltage)/(float)100;
+ 
+  Serial.println("Verdi lest fra AD omformer: "  + String(valueFromADConverter));
+  Serial.println("Temperatur: " + String(temperature) + " grader");
+  Serial.println("Spenning: " + String(voltage) + "v");
+  Serial.println();
+
+  // Vent litt før du leser den neste verdien (valgfritt)
+  delay(1000);
+}
